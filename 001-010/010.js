9b268ddab79612945ee36b8f299b8e771bcf33e7
github username <olshevskaia.anna@gmail.com>
2017-09-23 17:12:10 -0400

    challenge 10 solved

--- 001-010/010.js
+++ 001-010/010.js
@@ -1,39 +1,30 @@
 // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 // Find the sum of all the primes below two million.
 
-function isPrime(num){
-  for(let i = 2; i < num / 2; i++){
-    if(num % i === 0){
-      return false
+function problem010(n){
+  // Eratosthenes algorithm to find all primes under n
+  var array = [], upperLimit = Math.sqrt(n), sum = 0;
+
+  // Make an array from 2 to (n - 1)
+  for (var i = 0; i < n; i++) {
+    array.push(true);
+  }
+
+  // Remove multiples of primes starting from 2, 3, 5,...
+  for (var i = 2; i <= upperLimit; i++) {
+    if (array[i]) {
+      for (var j = i * i; j < n; j += i) {
+          array[j] = false;
+      }
     }
   }
-  return true
-}
 
-function problem010() {
-  // var primes = [2]
-  // var sum = 0
-  // for (let i = 3; i <= 2000000/2 - 1; i+=2){
-  //   if(isPrime(i)){
-  //     sum +=i
-  //   }
-  // }
-  // for (let i = 2000000/2 + 1; i< 2000000; i +=2){
-  //   if(isPrime(i)){
-  //     sum +=i
-  //   }
-  // }
-  // return sum
-  var allNumbers = () => {
-    var allNum = [2]
-    for(let i = 3 )
-  }
-  function getSum(array) {
-
+  for (var i = 2; i < n; i++) {
+    if(array[i]) {
+      sum += i
+    }
   }
 
-
+  return sum;
 }
-
-
-console.log(problem010());
+console.log(problem010(2000000));
